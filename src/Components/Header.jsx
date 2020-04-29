d81527c2bcc5e5725f6433b70a8f153e6dc094d3
Costa <costyanov@gmail.com>
2020-04-29 12:29:30 +0300

    first commit lesson 12 completed

---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className='header'>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTtIxS5TspYgEjhyCNTpwxWc0ag7lAF5KJ9dRQwDCwUGBQufa3a&usqp=CAU" />
+    </header>
+  );
+
+}
+
+export default Header;
