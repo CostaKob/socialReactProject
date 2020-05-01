f04886729bc4858d78cd5bff7269b4ccf60db04e
Costa <costyanov@gmail.com>
2020-05-01 09:25:20 +0300

    lesson 18 completed

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -8,8 +8,8 @@ const Navbar = () => {
   return (
     
     <nav className={classes.nav}>
-    <div className={`${classes.item} ${classes.active}`}><a>Profile</a></div>
-    <div className={classes.item}><a>Messages</a></div>
+    <div className={`${classes.item} ${classes.active}`}><a href="/profile">Profile</a></div>
+    <div className={classes.item}><a href="/dialogs">Messages</a></div>
     <div className={classes.item}><a>News</a></div>
     <div className={classes.item}><a>Music</a></div>
     <div className={classes.item}><a>Settings</a></div>
