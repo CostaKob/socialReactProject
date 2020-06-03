7642f60c386f19839655d3ff4382a660a63ce7fe
Costa <costyanov@gmail.com>
2020-06-03 17:06:21 +0300

    lesson 85 completed HOOKS useEffect

--- src/Components/Profile/ProfileStatusWithHooks.jsx
+++ src/Components/Profile/ProfileStatusWithHooks.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import classes from './Profile.module.css';
 
 // let arr = [0, () => {}]; destructuring assigment
@@ -9,30 +9,36 @@ const ProfileStatusWithHooks = (props) => {
     let [editMode, setEditMode] = useState(false);  // Use State returns [a, setA]
     let [status, setStatus] = useState(props.status);
 
+    useEffect(() => {
+        setStatus(props.status);
+    }, [props.status]); // after render
+
     const activateEditMode = () => {
         setEditMode(true);
     };
+
     const deactivateEditMode = () => {
         setEditMode(false);
         props.updateStatusThunk(status);
     };
+
     const onStatusChange = (e) => {
-        setStatus (e.currentTarget.value);
+        setStatus(e.currentTarget.value);
     };
 
     return (
         <div>
-            { !editMode &&
+            {!editMode &&
                 <div>
-                    <span onDoubleClick={ activateEditMode }>{props.status || "----------"}</span>
+                    <span onDoubleClick={activateEditMode}>{props.status || "----------"}</span>
                 </div>
             }
-            { editMode &&
+            {editMode &&
                 <div>
-                    <input onChange={onStatusChange} 
-                           autoFocus={true} 
-                           onBlur={deactivateEditMode}
-                           value={status}/>
+                    <input onChange={onStatusChange}
+                        autoFocus={true}
+                        onBlur={deactivateEditMode}
+                        value={status} />
                 </div>
             }
         </div>
