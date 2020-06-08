6ba3673ed6bbce1040a2322a04755727a2710a36
Costa <costyanov@gmail.com>
2020-06-08 18:28:58 +0300

    Updates

---
+++ precache-manifest.9b556d98a4a06b380dd3b9adc2e72aba.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfecedda6f6053b00fa8e1b8ffa5be70",
+    "url": "/socialReactProject/index.html"
+  },
+  {
+    "revision": "18c9727e2d7df28f892a",
+    "url": "/socialReactProject/static/css/3.85ef809e.chunk.css"
+  },
+  {
+    "revision": "36adf493892abb388606",
+    "url": "/socialReactProject/static/css/4.a6cb786e.chunk.css"
+  },
+  {
+    "revision": "c989f3f030be19b431b5",
+    "url": "/socialReactProject/static/css/main.4240cf4b.chunk.css"
+  },
+  {
+    "revision": "2fa28ff2c9b9daba1df4",
+    "url": "/socialReactProject/static/js/2.a71f23af.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/socialReactProject/static/js/2.a71f23af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18c9727e2d7df28f892a",
+    "url": "/socialReactProject/static/js/3.517aec0a.chunk.js"
+  },
+  {
+    "revision": "36adf493892abb388606",
+    "url": "/socialReactProject/static/js/4.9c26c6a9.chunk.js"
+  },
+  {
+    "revision": "c989f3f030be19b431b5",
+    "url": "/socialReactProject/static/js/main.7d47e18b.chunk.js"
+  },
+  {
+    "revision": "4a20e1aaada3c5ab25e7",
+    "url": "/socialReactProject/static/js/runtime-main.9965d4b1.js"
+  },
+  {
+    "revision": "49c8c1d4cf72f3c02145814516c7bc9c",
+    "url": "/socialReactProject/static/media/preloader.49c8c1d4.svg"
+  },
+  {
+    "revision": "129ab51cf970757975b12fd31c5223c6",
+    "url": "/socialReactProject/static/media/user.129ab51c.png"
+  }
+]);
